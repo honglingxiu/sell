341d6e55b097b4fe9f7eedca017754fdf3468239
hlx <1197379076@qq.com>
2018-07-05 15:25:44 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <i class="iconfont icon-zudui"></i>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+    data () {
+      return {}
+    }
+  }
+</script>
+<style lang="stylus" scoped>
+</style>
